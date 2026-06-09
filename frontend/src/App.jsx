4763200c9d38fef1610a0819bacf4485926ae099
AmanRajSharma001 <amanrajsharma699@gmail.com>
2026-06-09 17:31:28 +0530

    updating this part

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 
-
 function App() {
   const [count, setCount] = useState(0)
-
   return (
-  <>
-
     <div>
-      hello
+      <h1>{count}</h1>
+      <button onClick={()=>setCount(count+1)}>Add to Count</button>
     </div>
-    </>
   )
 }
 
